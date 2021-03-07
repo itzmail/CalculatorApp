a9de63bdd9aeebb3cfe425980bfc6b0c601284d3
ismailify <ismailnuralam@gmail.com>
2021-03-07 16:17:25 +0700

    Almost finish

--- App.js
+++ App.js
@@ -6,20 +6,40 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      calculationText: ""
+      calculationText: "",
+      resultText: ''
     }
+    this.operation = ['Del', '+', '-', '*', '/'];
   }
 
   calculateResult() {
     const text = this.state.calculationText
+    console.log(text, eval(text))
+    this.setState({
+      resultText: eval(text)
+    })
+    //BODMAS
+    // eval(text)
     // now parse this text
+  }
+
+  validate() {
+    const text = this.state.calculationText
+    switch(text.slice(-1)) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+          return false
+    }
+    return true
   }
 
   buttonbPressed(text) {
     //console.log(text)
 
     if(text == '=') {
-      return this.calculateResult()
+      return this.validate() && this.calculateResult()
     }
     this.setState({
         calculationText: this.state.calculationText + text
@@ -31,9 +51,21 @@ class App extends React.Component {
       case 'Del' :
         let text = this.state.calculationText.split('')
         text.pop()
-        
         this.setState({
           calculationText: text.join('')
+        })
+      break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        const lastChar = this.state.calculationText.split('').pop()
+
+        if(this.operation.indexOf(lastChar) > 0) return
+
+        if(this.state.text == "") return
+        this.setState({
+          calculationText: this.state.calculationText + operation
         })
     }
   }
@@ -54,11 +86,10 @@ class App extends React.Component {
     }
 
     let oper = [];
-    let operation = ['Del', '+', '-', '*', '/'];
-    for(let i = 0; i < 4; i++) {
+    for(let i = 0; i < 5; i++) {
       oper.push(
-      <TouchableOpacity style={styles.btn} onPress={() => this.operate(operation[i])}>
-        <Text style={[styles.btnText, {color: 'white'}]}>{operation[i]}</Text>
+      <TouchableOpacity style={styles.btn} onPress={() => this.operate(this.operation[i])}>
+        <Text style={[styles.btnText, {color: 'white'}]}>{this.operation[i]}</Text>
       </TouchableOpacity>
       )
     }
@@ -68,7 +99,7 @@ class App extends React.Component {
             <Text style={styles.calculationText}>{this.state.calculationText}</Text>
           </View>
           <View style={styles.result}>
-            <Text style={styles.resultText}>121</Text>
+            <Text style={styles.resultText}>{this.state.resultText}</Text>
           </View>
           <View style={styles.buttons}>
             <View style={styles.numbers}>
